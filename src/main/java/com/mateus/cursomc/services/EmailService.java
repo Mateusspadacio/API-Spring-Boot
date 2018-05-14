61a92a9c6c94b4f5e9577f9f7fe2a9addbefc13a
Mateus Renato <mateusspadacio@gmail.com>
2018-05-14 17:01:25 -0300

    Email HTML

--- src/main/java/com/mateus/cursomc/services/EmailService.java
+++ src/main/java/com/mateus/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.mateus.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.mateus.cursomc.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	public void sendOrderConfirmationEmail(Pedido obj);
 	
 	public void sendEmail(SimpleMailMessage msg);
+	
+	public void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	public void sendHtmlEmail(MimeMessage msg);
 	
 }
