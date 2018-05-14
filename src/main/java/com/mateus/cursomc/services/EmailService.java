b34482a2f90f4f2e959d463b29ab15cda4cdae41
Mateus Renato <mateusspadacio@gmail.com>
2018-05-14 15:17:23 -0300

    MockEmailService com Logger. Padrões Strategy e Template

---
+++ src/main/java/com/mateus/cursomc/services/EmailService.java
@@ -0,0 +1,13 @@
+package com.mateus.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.mateus.cursomc.domain.Pedido;
+
+public interface EmailService {
+
+	public void sendOrderConfirmationEmail(Pedido obj);
+	
+	public void sendEmail(SimpleMailMessage msg);
+	
+}
