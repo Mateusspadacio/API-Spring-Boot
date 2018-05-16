5a8ca581617aff5350b28e1442ff6006a871d729
Mateus Renato <mateusspadacio@gmail.com>
2018-05-16 15:19:34 -0300

    Expondo o header authorization

--- src/main/java/com/mateus/cursomc/resources/AuthResource.java
+++ src/main/java/com/mateus/cursomc/resources/AuthResource.java
@@ -22,18 +22,19 @@ public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
-	
+
 	@Autowired
 	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
-	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {	
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
 	public ResponseEntity<Void> forgotPassword(@Valid @RequestBody EmailDTO cliente) {
 		service.sendNewPassword(cliente.getEmail());
