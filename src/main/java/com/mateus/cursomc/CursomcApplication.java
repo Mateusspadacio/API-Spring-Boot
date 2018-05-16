f86946e9c506f54dfa60728e33b0896086cc8379
Mateus Renato <mateusspadacio@gmail.com>
2018-05-16 12:39:17 -0300

    Sanvando imagens no bucket Amazon s3

--- src/main/java/com/mateus/cursomc/CursomcApplication.java
+++ src/main/java/com/mateus/cursomc/CursomcApplication.java
@@ -1,41 +1,12 @@
 package com.mateus.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.mateus.cursomc.domain.Categoria;
-import com.mateus.cursomc.domain.Cidade;
-import com.mateus.cursomc.domain.Cliente;
-import com.mateus.cursomc.domain.Endereco;
-import com.mateus.cursomc.domain.Estado;
-import com.mateus.cursomc.domain.ItemPedido;
-import com.mateus.cursomc.domain.Pagamento;
-import com.mateus.cursomc.domain.PagamentoComBoleto;
-import com.mateus.cursomc.domain.PagamentoComCartao;
-import com.mateus.cursomc.domain.Pedido;
-import com.mateus.cursomc.domain.Produto;
-import com.mateus.cursomc.domain.enums.EstadoPagamento;
-import com.mateus.cursomc.domain.enums.TipoCliente;
-import com.mateus.cursomc.repositories.CategoriaRepository;
-import com.mateus.cursomc.repositories.CidadeRepository;
-import com.mateus.cursomc.repositories.ClienteRepository;
-import com.mateus.cursomc.repositories.EnderecoRepository;
-import com.mateus.cursomc.repositories.EstadoRepository;
-import com.mateus.cursomc.repositories.ItemPedidoRepository;
-import com.mateus.cursomc.repositories.PagamentoRepository;
-import com.mateus.cursomc.repositories.PedidoRepository;
-import com.mateus.cursomc.repositories.ProdutoRepository;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
-	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -44,6 +15,6 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 	}	
 }
