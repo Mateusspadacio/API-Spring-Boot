c41918f5822e6575980e52d5201f3a5970614499
Mateus Renato <mateusspadacio@gmail.com>
2018-05-16 22:37:03 -0300

    Padronizando excecoes de error

--- src/main/java/com/mateus/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/mateus/cursomc/resources/exception/ValidationError.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ValidationError extends StandardError {
-
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
-	}
+	private static final long serialVersionUID = 1L;
 	
 	private List<FieldMessage> errors = new ArrayList<>();
-
-	private static final long serialVersionUID = 1L;
+	
+	public ValidationError(Long timeStamp, Integer status, String error, String message, String path) {
+		super(timeStamp, status, error, message, path);
+	}
 
 	public List<FieldMessage> getErrors() {
 		return errors;
